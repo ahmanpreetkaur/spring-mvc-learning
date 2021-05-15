bc51e59ba71b0dbf1f81b5259919f614cdd889cb
ahmanpreetkaur <ahmanpreet.kaur@catalystone.com>
2021-05-15 23:00:19 +0530

    Learning about form tags in Spring MVC: drop-down lists

--- WebContent/WEB-INF/view/student-confirmation.jsp
+++ WebContent/WEB-INF/view/student-confirmation.jsp
@@ -7,7 +7,10 @@
 </head>
 
 <body>
-	The student is confirmed: ${student.firstName} ${student.lastName}	
+	The student is confirmed: ${student.firstName} ${student.lastName}
+	<br><br>
+	
+	Country: ${student.country}
 </body>
 
 </html>
--- src/com/springudemy/learning/mvc/Student.java
+++ src/com/springudemy/learning/mvc/Student.java
@@ -1,13 +1,28 @@
 package com.springudemy.learning.mvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Student {
 	private String firstName;
 	private String lastName;
+	private String country;
+	
+	private Map<String, String> countryOptions;
 	
 	public Student() {
-		
+		countryOptions = new HashMap<>();
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("IN", "India");
+		countryOptions.put("CA", "Canada");
+		countryOptions.put("US", "United States");
 	}
 	
+	public Map<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -19,5 +34,12 @@ public class Student {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
 	}
 }
